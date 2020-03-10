3885d94af9a9349fd9b94a3a7c1377030a7f22ce
Yanling Zhang <214670181@qq.com>
2020-03-10 21:03:16 +0800

    my data

---
+++ data/synthetic/plot.m
@@ -0,0 +1,12 @@
+plot(m1(:,1),m1(:,2),['.','r']);
+hold on
+plot(m2(:,1),m2(:,2),['.','b']);
+hold on
+plot(rc1(:,1),rc1(:,2),['.','g']);
+hold on
+plot(rc2(:,1),rc2(:,2),['.','y']);
+hold on
+plot(rc3(:,1),rc3(:,2),['.','c']);
+hold on
+plot(rc4(:,1),rc4(:,2),['.','m']);
+hold on
