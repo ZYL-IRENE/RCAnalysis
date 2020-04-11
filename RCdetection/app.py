3b288ebcd9c64a457baf45bf14a38f42b170e577
Yanling Zhang <214670181@qq.com>
2020-04-11 22:00:43 +0800

    Add files via upload

--- RCdetection/app.py
+++ RCdetection/app.py
@@ -1,10 +1,11 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, jsonify
 import csv
 import copy
 import numpy as np
 import mymds
 import lof
 import feature_explore
+import category_analysis as ca
 
 app = Flask(__name__)
 
@@ -31,7 +32,7 @@ def index():
     print("data loaded!")
 
     # calculate feature distribution
-    (feature_position, feature_position_min, feature_position_max, feature_variance) = feature_explore.feature_analysis(
+    (feature_position, feature_position_min, feature_position_max, feature_variance, feature_mean) = feature_explore.feature_analysis(
         data)
 
     # initialize LOF class
@@ -44,12 +45,52 @@ def index():
     normalized_data_array = np.array(l.instances)
     (data_position, data_position_min, data_position_max) = mymds.get_position(normalized_data_array, 2)
 
+    '''print("start to compute RC")
+
+    rare_centers = lof.outliers(data, k_list)
+
+    center = rare_centers[0] 
+    center_index = center["index"]
+    center_kinf = center["k_inf"]
+    # center point and its k-neighbours
+    neighbours = lof.get_neighbours(center_kinf, data[center_index], data) 
+    # put togather as RC
+    category = copy.deepcopy(neighbours)
+    category.append(data[center_index])
+    #print(category)
+
+    category_mean = ca.category_mean_relative(category,feature_mean)'''
+    category_mean = [-3.066666666666663, -0.8555555555555556, -0.8944444444444457, -0.1222222222222222, 3.811111111111117, 5.094444444444445, 2.1611111111111114, -4.916666666666664, -7.033333333333333]
+    print(category_mean)
+
     return_data = {"raw_data": data, "feature_position": feature_position, "feature_position_min": feature_position_min,
                    "feature_position_max": feature_position_max, "feature_variance": feature_variance,
                    "feature_name": feature_name, "data_position": data_position, "data_position_min": data_position_min,
-                   "data_position_max": data_position_max}
+                   "data_position_max": data_position_max, "category_mean":category_mean}
+
+
+
+        ######### POST request #########
+    '''if request.method == "POST":
+        print("This is post")
+        list_add = []
+        getdata = json.loads(request.get_data())["data"]
+        print(getdata)'''
+
+    print("This is post")
+    recv_data = request.get_data()
+    print(recv_data)
+
+
     return render_template('index.html', data=return_data)
 
-
+'''
+@app.route('/new',methods=['POST'])  
+def new():
+    recv_data = request.get_data() 
+    print(recv_data)
+   
+    return recv_data                 
+'''
 if __name__ == '__main__':
     app.run()
